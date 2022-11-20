d9e69571d6887a707a3adf78e85cad36dc70ac29
MateuszSzymanski <szymanski.mateusz133@gmail.com>
2022-11-20 23:11:01 +0100

    Test: Unit testing service registration in new projects

---
+++ Backend/ProfileService/tests/LostAndFound.ProfileService.UnitTests/ServiceRegistrations/ProfileServiceThirdPartyServicesRegistrationTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+
+namespace LostAndFound.ProfileService.UnitTests.ServiceRegistrations
+{
+    public class ProfileServiceThirdPartyServicesRegistrationTests
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ServiceCollection _services;
+        private readonly Dictionary<string, string> _testCustomConfiguration;
+
+        public ProfileServiceThirdPartyServicesRegistrationTests()
+        {
+            _testCustomConfiguration = new Dictionary<string, string>
+            {
+                {
+                    "LostAndFoundBlobStorageSettings:ConnectionString",
+                    "mongodb://localhost:27017"
+                }
+            };
+            _configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(_testCustomConfiguration)
+                .Build();
+
+            _services = new ServiceCollection();
+        }
+    }
+}
