3b82783c08d97c2440bde7e236d3ff384f538a50
MateuszSzymanski <szymanski.mateusz133@gmail.com>
2022-12-09 21:23:12 +0100

    Test: Mapping profiles unit tests

--- Backend/PublicationService/tests/LostAndFound.PublicationService.UnitTests/Core/MappingProfiles/PublicationTypeEntityMappingProfileTests.cs
+++ Backend/PublicationService/tests/LostAndFound.PublicationService.UnitTests/Core/MappingProfiles/PublicationTypeEntityMappingProfileTests.cs
@@ -1,13 +1,18 @@
 ﻿using AutoMapper;
+using FluentAssertions;
 using LostAndFound.PublicationService.Core.MappingProfiles;
 using Xunit;
+using PublicationDtoType = LostAndFound.PublicationService.CoreLibrary.Enums.PublicationType;
+using PublicationEntityType = LostAndFound.PublicationService.DataAccess.Entities.PublicationEnums.Type;
+
 
 namespace LostAndFound.PublicationService.UnitTests.Core.MappingProfiles
 {
     public class PublicationTypeEntityMappingProfileTests
     {
-        [Fact]
-        public void ValidateProfileCommentEntityMappingProfileIsValid()
+        private readonly IMapper _mapper;
+
+        public PublicationTypeEntityMappingProfileTests()
         {
             MapperConfiguration mapperConfig = new(
                 cfg =>
@@ -15,9 +20,35 @@ namespace LostAndFound.PublicationService.UnitTests.Core.MappingProfiles
                     cfg.AddProfile(new PublicationTypeEntityMappingProfile());
                 });
 
-            IMapper mapper = new Mapper(mapperConfig);
+            _mapper = new Mapper(mapperConfig);
+        }
 
-            mapper.ConfigurationProvider.AssertConfigurationIsValid();
+        [Fact]
+        public void ValidateProfileCommentEntityMappingProfileIsValid()
+        {
+            _mapper.ConfigurationProvider.AssertConfigurationIsValid();
+        }
+
+        [Theory]
+        [InlineData(PublicationEntityType.LostSubject, PublicationDtoType.LostSubject)]
+        [InlineData(PublicationEntityType.FoundSubject, PublicationDtoType.FoundSubject)]
+        public void AutoMapper_ConvertFromPublicationEntityType_IsValid(
+            PublicationEntityType entityType, PublicationDtoType expectedPublicationType)
+        {
+            var value = _mapper.Map<PublicationDtoType>(entityType);
+
+            value.Should().Be(expectedPublicationType);
+        }
+
+        [Theory]
+        [InlineData(PublicationDtoType.LostSubject, PublicationEntityType.LostSubject)]
+        [InlineData(PublicationDtoType.FoundSubject, PublicationEntityType.FoundSubject)]
+        public void AutoMapper_ConvertFromPublicationDtoType_IsValid(
+            PublicationDtoType dtoType, PublicationEntityType expectedEntityPublicationType)
+        {
+            var value = _mapper.Map<PublicationEntityType>(dtoType);
+
+            value.Should().Be(expectedEntityPublicationType);
         }
     }
 }
