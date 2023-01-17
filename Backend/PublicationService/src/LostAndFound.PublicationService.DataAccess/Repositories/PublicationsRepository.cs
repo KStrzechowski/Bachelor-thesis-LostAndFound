d8db026e79236f731a53fe3143816ea81aa0d833
MateuszSzymanski <szymanski.mateusz133@gmail.com>
2023-01-17 19:24:07 +0100

    Fix: Search query not case sensitive

--- Backend/PublicationService/src/LostAndFound.PublicationService.DataAccess/Repositories/PublicationsRepository.cs
+++ Backend/PublicationService/src/LostAndFound.PublicationService.DataAccess/Repositories/PublicationsRepository.cs
@@ -152,8 +152,8 @@ namespace LostAndFound.PublicationService.DataAccess.Repositories
 
             if (!String.IsNullOrEmpty(resourceParameters.SearchQuery))
             {
-                filter &= (builder.Regex(pub => pub.Description, new BsonRegularExpression($"/{resourceParameters.SearchQuery}/")) |
-                    builder.Regex(pub => pub.Title, new BsonRegularExpression($"/{resourceParameters.SearchQuery}/")));
+                filter &= (builder.Regex(pub => pub.Description.ToLower(), new BsonRegularExpression($"/{resourceParameters.SearchQuery.ToLower()}/")) |
+                    builder.Regex(pub => pub.Title.ToLower(), new BsonRegularExpression($"/{resourceParameters.SearchQuery.ToLower()}/")));
             }
 
             if (resourceParameters.CoordinateBoundaries is not null)
